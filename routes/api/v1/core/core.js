a07377ee25492f2aad3a0545f849129f7262a925
Michael Westbrooks <mwestbrooks@salesforce.com>
2020-05-29 23:18:33 -0400

    Updating api endpoint to save_meeting

--- routes/api/v1/core/core.js
+++ routes/api/v1/core/core.js
@@ -9,6 +9,21 @@ const formidable                        = require('formidable');
 const _                                 = require('underscore');
 const mime                              = require('mime');
 const randomstring                      = require('randomstring');
+const jwt                               = require('jsonwebtoken');
+const rp                                = require('request-promise');
+
+var Zoom = require('zoomus')({
+    key: 'z8O78FV9TtG8H9lIxqwR6w',
+    secret: 'jtNg8JEVVPJKCUy40U8qRUktJ37fuzwBglQF'
+   });
+
+   //Use the ApiKey and APISecret from config.js
+const payload = {
+    iss: 'z8O78FV9TtG8H9lIxqwR6w',
+    exp: ((new Date()).getTime() + 5000)
+};
+const token = jwt.sign(payload, 'jtNg8JEVVPJKCUy40U8qRUktJ37fuzwBglQF');
+
 
 router.use(bodyParser.json({ limit: '500mb' }));
 router.use(bodyParser.urlencoded({ limit: '500mb', extended: true, parameterLimit: 50000 }));
@@ -40,6 +55,112 @@ router.post('/eggman', function(req, res) {
                     "error_message": error.message
                 });
             });
+        });
+    }
+
+    if (action == 'save_meeting') {
+        // start_date
+        // end_date
+        // time_zone
+        // meeting_location
+        // meeting_type
+        // meeting_description
+        // agenda_items
+        // meeting_name
+        let data = JSON.parse(req.body.data);
+
+        if (!data.start_date || !data.end_date || !data.meeting_name || !data.owner) return res.status(200).json({
+            "status": 200,
+            "success": false,
+            "data": null,
+            "error_message": "1 or more parameters are missing. Please try again."
+        });
+
+        getFirebaseFirStorageInstance(res, function(reference) {
+            let refCollection = reference.collection('meetings');
+            refCollection.add(data).then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
+                data.key = docRef.id;
+                res.status(200).json({
+                    "status": 200,
+                    "success": true,
+                    "data": data,
+                    "error_message": null
+                });
+            }).catch(function (error) {
+                // arrayOfErrors.push(error.message);
+                res.status(200).json({
+                    "status": 200,
+                    "success": false,
+                    "data": null,
+                    "error_message": error.message
+                });
+            });
+        });
+    }
+
+    if (action =='createMeeting') {
+        email = req.body.email;
+        var options = {
+            method: "POST",
+            uri: "https://api.zoom.us/v2/users/" + email + "/meetings",
+            body: {
+                topic: "test create meeting",
+                type: 1,
+                settings: {
+                    host_video: "true",
+                    participant_video: "true"
+                }
+            },
+            auth: {
+                bearer: token
+            },
+            headers: {
+                "User-Agent": "Zoom-api-Jwt-Request",
+                "content-type": "application/json"
+            },
+            json: true //Parse the JSON string in the response
+        };
+
+        rp(options).then(function(response) {
+            console.log("response is: ", response);
+            res.send("create meeting result: " + JSON.stringify(response));
+        }).catch(function(err) {
+            // API call failed...
+            console.log("API call failed, reason ", err);
+        });
+    }
+
+    if (action =='createUser') {
+        email = req.body.email;
+        var options = {
+            method: "POST",
+            uri: "https://api.zoom.us/v2/users",
+            body: {
+                "action": "create",
+                "user_info": {
+                    "email": email,
+                    "type": 1,
+                    "first_name": "Terry",
+                    "last_name": "Jones"
+                }
+            },
+            auth: {
+                bearer: token
+            },
+            headers: {
+                "User-Agent": "Zoom-api-Jwt-Request",
+                "content-type": "application/json"
+            },
+            json: true //Parse the JSON string in the response
+        };
+
+        rp(options).then(function(response) {
+            console.log("response is: ", response);
+            res.send("create meeting result: " + JSON.stringify(response));
+        }).catch(function(err) {
+            // API call failed...
+            console.log("API call failed, reason ", err);
         });
     }
 }); 
