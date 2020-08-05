232a7a264c5d38a81b5f0727fb3a5691b2183300
Michael Westbrooks <mwestbrooks@salesforce.com>
2020-08-05 18:57:50 -0400

    Fixed declined meeting logic.

--- routes/api/v1/authentication/api_auth.js
+++ routes/api/v1/authentication/api_auth.js
@@ -570,7 +570,73 @@ function retrieveMeetings(uid, reference, completionHandler) {
                             callback(error, null);
                         }
                     });
-                }
+                },
+                participants: function(callback) {
+                    var participants = new Array();
+                    let prefCollection = reference.collection('users');
+                    async.forEachOf(userDoc.meeting_participants_ids, function(participantId, k, completion) {
+                        prefCollection.where('uid','==', participantId).get(getOptions).then(function(querysnapshot) {
+                            async.forEachOf(querysnapshot.docs, function(d, l, c) {
+                                var prefdata = d.data();
+                                prefdata.key = d.id;
+                                participants.push(prefdata);
+                                c();
+                            }, function(_e) {
+                                if (_e) { 
+                                    console.log(_e.message);
+                                    completion(_e, participants);
+                                } else {
+                                    completion(null, participants);
+                                }
+                            });
+                        }).catch(function (error) {
+                            if (error) {
+                                console.log(error.message);
+                                callback(error, null);
+                            }
+                        });
+                    }, function(_e) {
+                        if (_e) { 
+                            console.log(_e.message);
+                            callback(_e, participants);
+                        } else {
+                            callback(null, participants);
+                        }
+                    })
+                },
+                declined_participants: function(callback) {
+                    var participants = new Array();
+                    let prefCollection = reference.collection('users');
+                    async.forEachOf(userDoc.decline_meeting_participants_ids, function(participantId, k, completion) {
+                        prefCollection.where('uid','==', participantId).get(getOptions).then(function(querysnapshot) {
+                            async.forEachOf(querysnapshot.docs, function(d, l, c) {
+                                var prefdata = d.data();
+                                prefdata.key = d.id;
+                                participants.push(prefdata);
+                                c();
+                            }, function(_e) {
+                                if (_e) { 
+                                    console.log(_e.message);
+                                    completion(_e, participants);
+                                } else {
+                                    completion(null, participants);
+                                }
+                            });
+                        }).catch(function (error) {
+                            if (error) {
+                                console.log(error.message);
+                                callback(error, null);
+                            }
+                        });
+                    }, function(_e) {
+                        if (_e) { 
+                            console.log(_e.message);
+                            callback(_e, participants);
+                        } else {
+                            callback(null, participants);
+                        }
+                    })
+                },
             }, function(error, results) {
                 console.log(results);
                 console.log(error);
@@ -579,6 +645,14 @@ function retrieveMeetings(uid, reference, completionHandler) {
 
                 if (results.owner) {
                     userDoc.owner = results.owner
+                }
+
+                if (results.participants) {
+                    userDoc.participants = results.participants;
+                }
+
+                if (results.declined_participants) {
+                    userDoc.declined_participants = results.declined_participants;
                 }
 
                 users.push(userDoc);
